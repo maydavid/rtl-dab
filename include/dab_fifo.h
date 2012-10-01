e1031cd4bcab081d1824f5aa99469b96f7d23598
maydavid <david.may.muc@googlemail.com>
2012-10-01 14:27:37 +0200

    added fifo

---
+++ include/dab_fifo.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdint.h>
+
+
+typedef struct 
+{
+  uint32_t size;
+  uint32_t start;
+  uint32_t count;
+  uint8_t *elems;
+} CircularBuffer;
+
+
+void cbInit(CircularBuffer *cb, uint32_t size);
+void cbFree(CircularBuffer *cb);
+int cbIsFull(CircularBuffer *cb);
+int cbIsEmpty(CircularBuffer *cb);
+void cbWrite(CircularBuffer *cb, uint8_t *elem);
+void cbRead(CircularBuffer *cb, uint8_t *elem);
---
+++ src/dab_fifo.c
@@ -0,0 +1,41 @@
+#include "fifo.h"
+
+
+/* http://en.wikipedia.org/wiki/Circular_buffer */
+
+
+
+void cbInit(CircularBuffer *cb, uint32_t size) {
+    cb->size  = size;
+    cb->start = 0;
+    cb->count = 0;
+    cb->elems = (uint8_t *)calloc(cb->size, sizeof(uint8_t));
+}
+void cbFree(CircularBuffer *cb) {
+    free(cb->elems);
+	}
+
+int cbIsFull(CircularBuffer *cb) {
+    return cb->count == cb->size; 
+	}
+ 
+int cbIsEmpty(CircularBuffer *cb) {
+    return cb->count == 0;
+	 }
+ 
+void cbWrite(CircularBuffer *cb, uint8_t *elem) {
+    uint32_t end = (cb->start + cb->count) % cb->size;
+    cb->elems[end] = *elem;
+    if (cb->count == cb->size) {
+        cb->start = (cb->start + 1) % cb->size; 
+	fprintf(stderr,"fifo overflow!\n");
+    }
+    else
+        ++ cb->count;
+}
+ 
+void cbRead(CircularBuffer *cb, uint8_t *elem) {
+    *elem = cb->elems[cb->start];
+    cb->start = (cb->start + 1) % cb->size;
+    -- cb->count;
+}
