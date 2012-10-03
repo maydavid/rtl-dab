25fcc2a03c31ad21dbbff19260460988433111a1
David May <dave.may.muc@googlemail.com>
2012-10-03 16:48:44 +0200

    added coarse sync

--- src/dab_demod/dab_sync.c
+++ src/dab_demod/dab_sync.c
@@ -1,3 +1,24 @@
+/*
+This file is part of rtl-dab
+trl-dab is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Foobar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with rtl-dab.  If not, see <http://www.gnu.org/licenses/>.
+
+
+david may 2012
+david.may.muc@googlemail.com
+
+*/
+
 #include "dab_sync.h"
 
 uint32_t dab_coarse_time_sync(int8_t * real,uint8_t * buffer,float * filt) {
